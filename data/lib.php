f408fb9b4e7b78ec0e04738a7a16566d3a46bd6c
Качановский Евгений Сергеевич <djone4213@gmail.com>
2024-03-01 14:37:12 +0700

    first commit

---
+++ data/lib.php
@@ -0,0 +1,262 @@
+<?php
+
+use \Firebase\JWT\JWT;
+
+require_once 'jwt.lib.php';
+
+if (!isset($dirRoot)) {
+    $dirRoot = '';
+}
+
+//
+//  Config
+//
+
+class AppConfig {
+
+    var $appId = 'APP-ID';
+    var $appUid = 'APP-UID';
+    var $secretKey = 'SECRET-KEY';
+
+    var $appBaseUrl = 'APP-BASE-URL';
+
+    var $moyskladVendorApiEndpointUrl = 'https://apps-api.moysklad.ru/api/vendor/1.0';
+    var $moyskladJsonApiEndpointUrl = 'https://api.moysklad.ru/api/remap/1.2';
+
+    public function __construct(array $cfg)
+    {
+        foreach ($cfg as $k => $v) {
+            $this->$k = $v;
+        }
+    }
+}
+
+$cfg = new AppConfig(require('config.php'));
+
+function cfg(): AppConfig {
+    return $GLOBALS['cfg'];
+}
+
+//
+//  Vendor API 1.0
+//
+
+class VendorApi {
+
+    function context(string $contextKey) {
+        return $this->request('POST', '/context/' . $contextKey);
+    }
+
+    function updateAppStatus(string $appId, string $accountId, string $status) {
+        return $this->request('PUT',
+            "/apps/$appId/$accountId/status",
+            "{\"status\": \"$status\"}");
+    }
+
+    private function request(string $method, $path, $body = null) {
+        return makeHttpRequest(
+            $method,
+            cfg()->moyskladVendorApiEndpointUrl . $path,
+            buildJWT(),
+            $body);
+    }
+
+}
+
+function makeHttpRequest(string $method, string $url, string $bearerToken, $body = null) {
+    loginfo("APP => MOYSKLAD", "Send: $method $url\n$body");
+
+    $opts = $body
+        ? array('http' =>
+            array(
+                'method'  => $method,
+                'header'  => array('Authorization: Bearer ' . $bearerToken, "Accept-Encoding: gzip", "Content-type: application/json"),
+                'content' => $body
+            )
+        )
+        : array('http' =>
+            array(
+                'method'  => $method,
+                'header'  => array('Authorization: Bearer ' . $bearerToken, "Accept-Encoding: gzip")
+            )
+        );
+    $context = stream_context_create($opts);
+    $result = file_get_contents($url, false, $context);
+    return json_decode($result);
+}
+
+$vendorApi = new VendorApi();
+
+function vendorApi(): VendorApi {
+    return $GLOBALS['vendorApi'];
+}
+
+function buildJWT() {
+    $token = array(
+        "sub" => cfg()->appUid,
+        "iat" => time(),
+        "exp" => time() + 300,
+        "jti" => bin2hex(random_bytes(32))
+    );
+    return JWT::encode($token, cfg()->secretKey);
+}
+
+//
+//  JSON API 1.2
+//
+
+class JsonApi {
+
+    private $accessToken;
+
+    function __construct(string $accessToken) {
+        $this->accessToken = $accessToken;
+    }
+
+    function stores() {
+        return makeHttpRequest(
+            'GET',
+            cfg()->moyskladJsonApiEndpointUrl . '/entity/store',
+            $this->accessToken);
+    }
+
+    function getObject($entity, $objectId) {
+        return makeHttpRequest(
+            'GET',
+            cfg()->moyskladJsonApiEndpointUrl . "/entity/$entity/$objectId",
+            $this->accessToken);
+    }
+
+}
+
+function jsonApi(): JsonApi {
+    if (!$GLOBALS['jsonApi']) {
+        $GLOBALS['jsonApi'] = new JsonApi(AppInstance::get()->accessToken);
+    }
+    return $GLOBALS['jsonApi'];
+}
+
+//
+//  Logging
+//
+
+function loginfo($name, $msg) {
+    global $dirRoot;
+    $logDir = $dirRoot . 'logs';
+    @mkdir($logDir);
+    file_put_contents($logDir . '/log.txt', date(DATE_W3C) . ' [' . $name . '] '. $msg . "\n", FILE_APPEND);
+}
+
+//Проверка токена авторизации (при запросах со стороны МоегоСклада)
+function authTokenIsValid() {
+    $secretKey = cfg()->secretKey;
+    $headers = apache_request_headers();
+    if (!isset($headers['Authorization']) || empty($headers['Authorization'] || empty($secretKey))) {
+        return false;
+    }
+
+    $token = $headers['Authorization'];
+    if (strlen($token) == 0) {
+        return false;
+    }
+
+    $bearer = "Bearer ";
+    if (substr($token, 0, 7) != $bearer) {
+        return false;
+    }
+
+    $jwtToken = str_replace($bearer, "", $token);
+
+    try {
+        $decoded = JWT::decode($jwtToken, $secretKey, ["HS256"]);
+        if (empty($decoded->jti)) {
+            return false;
+        }
+        // jti - является уникальным идентификатором токена.
+        // Следовательно, нужно добавить проверку что ранее не было запроса с таким значением jti в токене
+        // @link - https://dev.moysklad.ru/doc/api/vendor/1.0/#autentifikaciq-wzaimodejstwiq-po-vendor-api
+        return true;
+    } catch (Exception $exception) {
+        //ToDo - Log the exception
+        return false;
+    }
+}
+
+//
+//  AppInstance state
+//
+
+$currentAppInstance = null;
+
+class AppInstance {
+
+    const UNKNOWN = 0;
+    const SETTINGS_REQUIRED = 1;
+    const ACTIVATED = 100;
+
+    var $appId;
+    var $accountId;
+    var $infoMessage;
+    var $store;
+
+    var $accessToken;
+
+    var $status = AppInstance::UNKNOWN;
+
+    static function get(): AppInstance {
+        $app = $GLOBALS['currentAppInstance'];
+        if (!$app) {
+            throw new InvalidArgumentException("There is no current app instance context");
+        }
+        return $app;
+    }
+
+    public function __construct($appId, $accountId)
+    {
+        $this->appId = $appId;
+        $this->accountId = $accountId;
+    }
+
+    function getStatusName() {
+        switch ($this->status) {
+            case self::SETTINGS_REQUIRED:
+                return 'SettingsRequired';
+            case self::ACTIVATED:
+                return 'Activated';
+        }
+        return null;
+    }
+
+    function persist() {
+        @mkdir('data');
+        file_put_contents($this->filename(), serialize($this));
+    }
+
+    function delete() {
+        @unlink($this->filename());
+    }
+
+    private function filename() {
+        return self::buildFilename($this->appId, $this->accountId);
+    }
+
+    private static function buildFilename($appId, $accountId) {
+        return $GLOBALS['dirRoot'] . "data/$appId.$accountId.app";
+    }
+
+    static function loadApp($accountId): AppInstance {
+        return self::load(cfg()->appId, $accountId);
+    }
+
+    static function load($appId, $accountId): AppInstance {
+        $data = @file_get_contents(self::buildFilename($appId, $accountId));
+        if ($data === false) {
+            $app = new AppInstance($appId, $accountId);
+        } else {
+            $app = unserialize($data);
+        }
+        $GLOBALS['currentAppInstance'] = $app;
+        return $app;
+    }
+
+}
